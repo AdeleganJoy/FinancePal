5d9555e707ff960d8c95b5f1a01d223c85d48033
Joy Adelegan <96977916+AdeleganJoy@users.noreply.github.com>
2025-03-16 07:24:24 +0200

    Update HomeController.cs
    Added Comments to improve readability

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,17 +27,21 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Index()
     {
+    //Check if a user is logged in by retrieving the username from the session 
        var logged_user = HttpContext.Session.GetString("LoggedInUser");
        var user = _context.Users.FirstOrDefault(u => u.Username == logged_user);
+       
+       // If the user is not logged in, redirect them to the login page
         if (string.IsNullOrEmpty(logged_user)){
             return RedirectToAction("Login");
         }
-
+        // Retrieve all expenses associated with the logged-in user, ordered by date descending
         var expenses = _context.Expenses
             .Where(e => e.UserId == user.Id)
             .OrderByDescending(e => e.Date) 
             .ToList();
 
+        // Group expenses by category and calculate the total amount for each category
         var categoryExpenseData = expenses
             .GroupBy(e => e.Category)
             .Select(g => new{
@@ -45,17 +49,19 @@ public class HomeController : Controller
                 TotalAmount = g.Sum(e => e.Amount)
             })
             .ToList();
-
+        
+        // Setup data for showing pie graph for Categories& Expenses
         var x = categoryExpenseData.Select(e => e.Category).ToArray();
         var y = categoryExpenseData.Select(e => e.TotalAmount).ToArray();
-        var barColors = new string[]
+        //Setup Color for Pie Chart
+        var pieColors = new string[]
         {
             "#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145", "#f39c12", "#8e44ad"
         };
-
+        //Send pie chart and User amount information to front-end
         ViewBag.XValues = x;
         ViewBag.YValues = y;
-        ViewBag.BarColors = barColors;
+        ViewBag.PieColors = pieColors;
         ViewBag.Balance = user.Amount;
 
         return View(expenses);
@@ -65,11 +71,13 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult Index(Expense model)
     {
+        //Check if a user is logged in by retrieving the username from the session 
         var logged_user = HttpContext.Session.GetString("LoggedInUser");
         var user = _context.Users.FirstOrDefault(u => u.Username == logged_user);
-
+        //Check if user exists
         if (user != null)
         {
+            //Change the balance
             if (model.Category == "salary")
             {
                 user.Amount = _financeCalculator.CalcBalance(user.Amount, model.Amount, model.Category); // Use FinanceCal
@@ -78,9 +86,11 @@ public class HomeController : Controller
             {
                 user.Amount = _financeCalculator.CalcBalance(user.Amount, model.Amount, model.Category); // Use FinanceCal
             }
+            //Update the User database
             _context.Users.Update(user);
             _context.SaveChanges();
 
+            //Update the Expense database
             model.UserId = user.Id;
             model.Date = DateTime.Now;
             _context.Expenses.Add(model);
@@ -107,20 +117,23 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult SignUp(User model){
         _logger.LogInformation($"Sign-Up Info: Username - {model.Username}, Email - {model.Email}, Amount - {model.Amount}");
+        //CHeck if user exists in system
         var user_exist = _context.Users.FirstOrDefault(u => u.Email == model.Email);
         if (user_exist == null)
         {
+            //Encrypt the password
             string salt;
             string hashedPassword = _authentifier.HashPassword(model.Password, out salt);
             model.Password = hashedPassword;
             model.Salt = salt;
 
+            //add the new user to the database
             _context.Users.Add(model);
             _context.SaveChanges();
 
             return RedirectToAction("Login");
         }
-
+        //Send error message to the frontend if the user exists
         ViewBag.ErrorMessage = "A user with this email already exists.";
         return View();
     }
@@ -136,19 +149,23 @@ public class HomeController : Controller
     public IActionResult Login(string username, string password){
         var user = _context.Users
             .FirstOrDefault(u => u.Username == username);
-
+            
+        //Check if username exists in the database
         if (user != null){
+            //Check if the password is correct
             if (_authentifier.ValidatePassword(password, user.Password, user.Salt)){
                 HttpContext.Session.SetString("LoggedInUser", user.Username);
                 return RedirectToAction("Index");
             }
         }
-        ViewBag.ErrorMessage = "The username or password is invalid";
+        ViewBag.ErrorMessage = "The username or password is invalid"; // Send error to the front end
         return View();
 
     }
-
+    //POST: Handling LogOut
+    [HttpPost]
     public IActionResult Logout(){
+        //End session for the logged user
         HttpContext.Session.Remove("LoggedInUser");
         return RedirectToAction("Login");
     }
